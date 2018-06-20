7404389bc23c6e99c60b6f26b19a830f7909828f
Pepps Persson <pepps.persson@pepps.se>
2018-06-20 21:18:33 +0200

    Refactored Add method

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -8,13 +8,9 @@ namespace ConsoleApp1
 {
     class Feature1
     {
-        public int Add()
+        public int Add(int x1, int x2)
         {
-            int x1 = 1;
-            int x2 = 1;
-            int sum = x1 + x2;
-
-            return sum;
+            return x1 + x2;
         }
     }
 }
